19eeb2393d0e8b08d01ddf126c574fc104b801c7
EsterKais <kaisester91@gmail.com>
2016-11-29 12:26:32 +0100

    Profile Creation Setup

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,26 @@
 class ProfilesController < ApplicationController
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = current_user.build_profile(profile_params)
+
+    if @profile.save
+      redirect_to profile_path(@profile), notice: "Profile Created - Welcome!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def set_profile
+      @profile = current_user.profile
+    end
+
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :bio)
+    end
 end
