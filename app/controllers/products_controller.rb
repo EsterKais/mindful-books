8ea1e34f41a80945a0287bf3666d04a9940fa6f3
EsterKais <kaisester91@gmail.com>
2016-12-02 16:12:27 +0100

    Show and edit actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+
+      redirect_to profile_path(@product.user.profile.id), notice: "Addition updated!"
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+
 end
