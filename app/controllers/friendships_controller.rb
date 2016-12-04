22d06bed143edb5f78b38faccec5a751ea57748a
EsterKais <kaisester91@gmail.com>
2016-12-04 16:00:47 +0100

    Unfriend setup

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,12 +4,19 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
       flash[:notice] = "Added friend!"
-      redirect_to root_path
+      redirect_to profile_path(current_user.profile.id)
     else
       flash[:notice] = "Unable to add friend."
       redirect_to root_path
     end
-    
+
+  end
+
+  def destroy
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Friend removed!"
+    redirect_to profile_path(current_user.profile.id)
   end
 
 end
